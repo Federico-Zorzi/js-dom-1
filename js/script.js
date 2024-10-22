489fd5d8fd790b8d393ccee6bd8f016748095b70
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-10-22 16:08:04 +0200

    background and btn modified

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
+const mySwitchContainer = document.getElementById("switch-container");
 const myLampImg = document.getElementById("lamp-img");
-const mySwitchBtn = document.getElementById("switch-btn");
+const mySwitchBtnId = document.getElementById("switch-btn");
+let mySwitchContainerClass = mySwitchContainer.classList;
+let mySwitchBtnClass = mySwitchBtnId.classList;
 
-console.log(myLampImg.classList);
-console.log(mySwitchBtn);
+let toggleSwitch = false;
 
-/* mySwitchBtn.addEventListener(() => {
-  myLampImg.classList;
-}); */
+mySwitchBtnId.addEventListener("click", () => {
+  if (!toggleSwitch) {
+    myLampImg.src = "./img/yellow_lamp.png";
+    myLampImg.alt = "lamp on";
+    mySwitchBtnId.textContent = "Spegni";
+    mySwitchBtnClass.replace("btn-outline-light", "btn-outline-dark");
+    mySwitchContainerClass.replace("bg-dark", "bg-white");
+    toggleSwitch = true;
+  } else {
+    myLampImg.src = "./img/white_lamp.png";
+    myLampImg.alt = "lamp off";
+    mySwitchBtnId.textContent = "Accendi";
+    mySwitchBtnClass.replace("btn-outline-dark", "btn-outline-light");
+    mySwitchContainerClass.replace("bg-white", "bg-dark");
+    toggleSwitch = false;
+  }
+});
