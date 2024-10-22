223a4f0c2c34f6925c071e5ca3c81935eb6d63ce
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-10-22 14:56:38 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const myLampImg = document.getElementById("lamp-img");
+const mySwitchBtn = document.getElementById("switch-btn");
+
+console.log(myLampImg.classList);
+console.log(mySwitchBtn);
+
+/* mySwitchBtn.addEventListener(() => {
+  myLampImg.classList;
+}); */
